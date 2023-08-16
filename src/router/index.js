3c0013ef3d3ea083938c637da4e5f2acbc2fc164
prakhar-8999 <prakharporwal12a@gmail.com>
2023-08-16 17:15:17 +0530

    initial project setup

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import RegisterView from '../views/RegisterView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'register',
+      component: RegisterView
+    },
+  ]
+})
+
+export default router
